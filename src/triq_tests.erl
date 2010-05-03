112e7fa629c39233d1ec31f82790d6292c1eb73c
Kresten Krab Thorup <krab@trifork.com>
2010-05-04 01:49:49 +0200

    Add license note

--- src/triq_tests.erl
+++ src/triq_tests.erl
@@ -1,14 +1,33 @@
+%%
+%% This file is part of Triq - Trifork QuickCheck
+%%
+%% Copyright (c) 2010 by Trifork
+%%
+%% Licensed under the Apache License, Version 2.0 (the "License");
+%% you may not use this file except in compliance with the License.
+%% You may obtain a copy of the License at
+%%  
+%%     http://www.apache.org/licenses/LICENSE-2.0
+%%
+%% Unless required by applicable law or agreed to in writing, software
+%% distributed under the License is distributed on an "AS IS" BASIS,
+%% WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+%% See the License for the specific language governing permissions and
+%% limitations under the License.
+%%
 
 %%
-%% Some simple tests
+%% This file contains some sample properties, which also
+%% function as a very simple test suite for Triq itself.
 %%
 
 -module(triq_tests).
 
+% including this also auto-exports all properties
 -include("triq.hrl").
-%-include("eunit.hrl").
 
-%-export([prop_append/0, prop_delete_2/0, prop_binop/0, prop_timeout/0]).
+% use eunit
+-include("eunit.hrl").
 
 %% eunit test!
 triq_test() ->
