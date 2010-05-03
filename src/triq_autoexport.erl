b07e3b9fb8e0f5572c2a0daabfa891a5e936b7e5
Kresten Krab Thorup <krab@trifork.com>
2010-05-04 01:49:29 +0200

    Make autoexport generate ?MODULE:check() method, not quickcheck.

--- src/triq_autoexport.erl
+++ src/triq_autoexport.erl
@@ -1,3 +1,14 @@
+%%
+%%
+%% For the purposes of licensing, I will permit myself to regard
+%% this module as a "separate library", because it is not linked
+%% with the resulting executable.  It is linked into the compiler,
+%% as it runs.  As such, a "GNU Lesser General Public License" is
+%% not really a problem; just make sure that you distribute this
+%% source code file along with any binaries.
+%%
+%% ----------------
+%%
 %% This library is free software; you can redistribute it and/or modify
 %% it under the terms of the GNU Lesser General Public License as
 %% published by the Free Software Foundation; either version 2 of the
@@ -17,7 +28,7 @@
 %% @copyright 2006 Richard Carlsson
 %% @private
 %% @see triq
-%% @doc Parse transform for automatic exporting of test functions.
+%% @doc Parse transform for automatic exporting of prop_ functions.
 
 -module(triq_autoexport).
 
@@ -25,6 +36,7 @@
 
 -export([parse_transform/2]).
 
+-define(CHECK,check).
 
 parse_transform(Forms, Options) ->
     PropPrefix = proplists:get_value(triq_prop_prefix, Options,
@@ -55,14 +67,14 @@ t_rewrite([F | Fs], Exports) ->
 t_rewrite([], _Exports) ->
     [].    %% fail-safe, in case there is no module declaration
 
-rewrite([{function,_,quickcheck,0,_}=F | Fs], As, Module, _GenQC) ->
+rewrite([{function,_,?CHECK,0,_}=F | Fs], As, Module, _GenQC) ->
     rewrite(Fs, [F | As], Module, false);
 rewrite([F | Fs], As, Module, GenQC) ->
     rewrite(Fs, [F | As], Module, GenQC);
 rewrite([], As, Module, GenQC) ->
     {if GenQC ->
 	     Triq = {record_field,0,{atom,0,''},{atom,0,triq}},
-	     [{function,0,quickcheck,0,
+	     [{function,0,?CHECK,0,
 	       [{clause,0,[],[],
 		 [{call,0,{remote,0,Triq,{atom,0,module}},
 		   [{atom,0,Module}]}]}]}
@@ -77,7 +89,7 @@ module_decl(Name, M, Fs, Exports) ->
 		true -> list_to_atom(packages:concat(Name))
 	     end,
     {Fs1, GenQC} = rewrite(Fs, [], Module, true),
-    Es = if GenQC -> [{quickcheck,0} | Exports];
+    Es = if GenQC -> [{?CHECK,0} | Exports];
 	    true -> Exports
 	 end,
     [M, {attribute,0,export,Es} | lists:reverse(Fs1)].
