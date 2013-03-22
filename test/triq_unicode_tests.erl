cbb0f3e0305a18fbb83d30fb5f2cc7c55ae5ca6e
Tuncer Ayaz <tuncer.ayaz@gmail.com>
2013-03-22 11:30:38 +0100

    Add missing copyright headers based on git history

--- test/triq_unicode_tests.erl
+++ test/triq_unicode_tests.erl
@@ -1,7 +1,24 @@
 %% -*- erlang-indent-level: 4;indent-tabs-mode: nil -*-
 %% ex: ts=4 sw=4 et
 %%
+%% This file is part of Triq - Trifork QuickCheck
+%%
+%% Copyright (c) 2013 by Uvarov Michael
+%%
+%% Licensed under the Apache License, Version 2.0 (the "License");
+%% you may not use this file except in compliance with the License.
+%% You may obtain a copy of the License at
+%%
+%%     http://www.apache.org/licenses/LICENSE-2.0
+%%
+%% Unless required by applicable law or agreed to in writing, software
+%% distributed under the License is distributed on an "AS IS" BASIS,
+%% WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+%% See the License for the specific language governing permissions and
+%% limitations under the License.
+%%
 %% @author Uvarov Michael <arcusfelis@gmail.com>
+
 -module(triq_unicode_tests).
 
 %% ------------------------------------------------------------------
